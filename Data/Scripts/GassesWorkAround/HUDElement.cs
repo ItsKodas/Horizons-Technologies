b7c76a9ff816c0a497d6a948952594a96744574f
Koda <koda@horizons.gg>
2023-10-05 20:24:19 +1000

    Tritium HUD Element
    Big thanks to @Afrayedknot for coding this one :)

--- Data/Scripts/GassesWorkAround/HUDElement.cs
+++ Data/Scripts/GassesWorkAround/HUDElement.cs
@@ -1,59 +1,158 @@
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
 using System;
 using Sandbox.ModAPI;
+using VRage.Game.Components;
 using VRage.Game;
 using VRage.ModAPI;
 using VRage.Utils;
 
-namespace Digi.Examples
+namespace Horizon.TritHUD
 {
     // HudStat classes are used by the game to compute data to be written to the various HUD elements.
     // Implementing IMyHudStat would trigger the game to use your class for that purpose aswell, and the Id property determines the stat it overrides (or creates if it's unique).
     // You can make new Ids and use them in a HUD definition SBC to have custom behaviors on the HUD.
     // However, do note that currently the HUD definition is very unfriendly to multiple mods changing it, only one mod's edits will remain depending on mod order.
     // 
-    public class Example_HudStatOverride : IMyHudStat
+    public class Trit_HudStatOverride : IMyHudStat
     {
         public MyStringHash Id { get; private set; } = MyStringHash.GetOrCompute("controlled_tritium_capacity"); // the stat's ID to override, this one is the ship's mass number
         public float MinValue => 0; // these being used or not depend on the how the stat is used in the HUD definition.
         public float MaxValue => 100;
         public float CurrentValue { get; private set; }
-        public string GetValueString() => CurrentValue.ToString("0.00"); // NOTE: must never return null!
+        public string GetValueString() => ((int)CurrentValue).ToString(); // NOTE: must never return null!
 
-        public Example_HudStatOverride()
+        public Trit_HudStatOverride()
         {
-            // initialization stuff
+            CurrentValue = 0;
         }
 
         public void Update() // gets executed every tick (60/s)
         {
-            try
-            {
-                
-                // ModAPI.IMyEntity ent = MyAPIGateway.Gui.InteractedEntity;
-                // if (ent is MyCubeGrid) {
-                    
-                //     MyCubeGrid grid = ent as MyCubeGrid;
-                //     var allblocks = grid.GetBlocks();
+            IMyCubeGrid grid;
 
-                //     foreach (var block in allblocks) {
-                //         if (block is IMyGasTank) {
-                //             IMyGasTank tank =  block as IMyGasTank;
-                //             if (tank.) {
+            if(MyAPIGateway.Session == null)
+                return;
 
-                //             }
-                //         }
-                //     }
-
-                // }
-
+            IMyEntity entity = MyAPIGateway.Session.Player.Controller.ControlledEntity.Entity;
+            if (entity is IMyCubeBlock) {
+                grid = (entity as IMyCubeBlock).CubeGrid;
             }
-            catch(Exception e)
+            else
             {
-                MyLog.Default.WriteLineAndConsole($"{e.Message}\n{e.StackTrace}");
-
-                if(MyAPIGateway.Session?.Player != null)
-                    MyAPIGateway.Utilities.ShowNotification($"[ ERROR: {GetType().FullName}: {e.Message} | Send SpaceEngineers.Log to mod author ]", 10000, MyFontEnum.Red);
+                return;
             }
+
+            CurrentValue = 0;
+            float total = 0;
+            float fill = 0;
+
+            MyDefinitionId TritiumDefId = MyDefinitionId.Parse("MyObjectBuilder_GasProperties/Tritium");
+
+            foreach (var block in grid.GetFatBlocks<IMyGasTank>()) {
+                // Need to consider blocks can have different capacities
+                var sink = block.Components.Get<MyResourceSinkComponent>();
+                if (sink != null && sink.AcceptedResources.IndexOf(TritiumDefId) != -1)
+                {
+                    total += block.Capacity;
+                    fill += block.Capacity * (float)block.FilledRatio;
+                }
+            }
+
+            // If total is 0, just set 0 and return early
+            if (total == 0) {
+                CurrentValue = 0;
+            }
+            else
+            {
+                // Calculate final capacity
+                CurrentValue = (fill / total) * 100;
+            }
+
+            // Should never be above 100
+            if (CurrentValue > 100)
+            {
+                CurrentValue = 100;
+            }
+
+            // Should never be below 0
+            if (CurrentValue < 0)
+            {
+                CurrentValue = 0;
+            }
+        }
+    }
+
+
+    // If CurrentValue = 1, then blink the Trit UI (because you are 25% fuel or less). But we need to check if they even had Trit tanks, otherwise dont make it red for a hydro only ship
+    public class Trit_HudStatWarning : IMyHudStat
+    {
+        public MyStringHash Id { get; private set; } = MyStringHash.GetOrCompute("tritium_warning_on"); // the stat's ID to override, this one is the ship's mass number
+        public float MinValue => 0; // these being used or not depend on the how the stat is used in the HUD definition.
+        public float MaxValue => 1;
+        public float CurrentValue { get; private set; }
+        public string GetValueString() => ((int)CurrentValue).ToString(); // NOTE: must never return null!
+
+        public Trit_HudStatWarning()
+        {
+            CurrentValue = 0;
+        }
+
+        public void Update() // gets executed every tick (60/s)
+        {
+            IMyCubeGrid grid;
+
+            if(MyAPIGateway.Session == null)
+                return;
+
+            IMyEntity entity = MyAPIGateway.Session.Player.Controller.ControlledEntity.Entity;
+            if (entity is IMyCubeBlock) {
+                grid = (entity as IMyCubeBlock).CubeGrid;
+            }
+            else
+            {
+                return;
+            }
+
+            CurrentValue = 0;
+            float current = 0;
+            float total_capacity = 0;
+            float fill_amount = 0;
+
+            MyDefinitionId TritiumDefId = MyDefinitionId.Parse("MyObjectBuilder_GasProperties/Tritium");
+
+            foreach (var block in grid.GetFatBlocks<IMyGasTank>()) {
+                // Need to consider blocks can have different capacities
+                var sink = block.Components.Get<MyResourceSinkComponent>();
+                if (sink != null && sink.AcceptedResources.IndexOf(TritiumDefId) != -1)
+                {
+                    total_capacity += block.Capacity;
+                    fill_amount += block.Capacity * (float)block.FilledRatio;
+                }
+            }
+
+            // If total is less than 1 - that means no Trit tanks, so dont blink
+            if (total_capacity < 1) {
+                CurrentValue = 0;
+            }
+            else
+            {
+                // Calculate final capacity
+                current = (fill_amount / total_capacity) * 100;
+
+                // If current less than 25% - then blink (since we have Trit tanks)
+                if (current < 25)
+                {
+                    CurrentValue = 1;
+                }
+                else
+                {
+                    CurrentValue = 0;
+                }
+            }
+
+
         }
     }
 }
